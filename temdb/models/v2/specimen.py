3d0b096b198a9a52bbc30aef1e77bbd75e62d0c6
dlbrittain <dlbrittain@gmail.com>
2024-09-06 13:28:12 -0700

    Feat: update specimen api

--- temdb/models/v2/specimen.py
+++ temdb/models/v2/specimen.py
@@ -1,4 +1,4 @@
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional,Set
 from beanie import Document
 from pymongo import IndexModel, ASCENDING, DESCENDING
 from pydantic import BaseModel
@@ -6,16 +6,16 @@ from datetime import datetime
 
 
 class SpecimenUpdate(BaseModel):
-    name: Optional[str] = None
+    specimen_id: Optional[str] = None
     description: Optional[str] = None
     specimen_images: Optional[List[str]] = None
     functional_imaging_metadata: Optional[Dict] = None
 
 
 class Specimen(Document):
-    name: str
-    description: str
-    specimen_images: List[str]
+    specimen_id: str
+    description: Optional[str]
+    specimen_images: Set[str]
     created_at: datetime
     updated_at: Optional[datetime]
     functional_imaging_metadata: Optional[Dict]
@@ -23,7 +23,7 @@ class Specimen(Document):
     class Settings:
         name = "specimens"
         indexes = [
-            IndexModel([("name", ASCENDING)], unique=True, name="name_index"),
+            IndexModel([("specimen_id", ASCENDING)], unique=True, name="specimen_id_index"),
             IndexModel([("created_at", DESCENDING)], name="created_at_index"),
             IndexModel([("updated_at", DESCENDING)], name="updated_at_index"),
         ]
