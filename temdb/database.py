7d3a4856079392f029ee7b60ecd39c3b53689552
dlbrittain <dlbrittain@gmail.com>
2024-09-06 13:26:56 -0700

    feat: add database manager and dynamic collection creation

--- temdb/database.py
+++ temdb/database.py
@@ -1,9 +1,10 @@
 import logging
+from typing import Type, TypeVar, List, Dict
 
 from motor.motor_asyncio import AsyncIOMotorClient
-from beanie import init_beanie
+from beanie import init_beanie, Document
 
-
+from copy import deepcopy
 from temdb.models.v1.grids import Grid
 
 from temdb.models.v2.specimen import Specimen
@@ -13,34 +14,66 @@ from temdb.models.v2.section import Section
 from temdb.models.v2.roi import ROI
 from temdb.models.v2.imaging_session import ImagingSession
 from temdb.models.v2.acquisition import Acquisition
-from temdb.models.v2.tile import Tile
-
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+TDocument = TypeVar("TDocument", bound=Document)
 
-async def init_db(mongo_url: str, db_name: str):
 
-    document_models = [
-        Grid, # original metadata schema
-        Specimen,
-        Block,
-        CuttingSession,
-        Section,
-        ROI,
-        ImagingSession,
-        Acquisition,
-        Tile,
-    ]
+class DatabaseManager:
 
-    try:
-        client = AsyncIOMotorClient(mongo_url)
-        db = client[db_name]
-        await init_beanie(db, document_models=document_models)
+    def __init__(self, mongo_url: str, db_name: str):
+        self.mongo_url = mongo_url
+        self.db_name = db_name
+        self.client = AsyncIOMotorClient(mongo_url)
+        self.db = self.client[db_name]
+        self._static_models: List[Type[Document]] = [
+            Grid,
+            Specimen,
+            Block,
+            CuttingSession,
+            Section,
+            ROI,
+            ImagingSession,
+            Acquisition,
+        ]
 
-        collections = await db.list_collection_names()
-        logger.debug(f"Collections in database: {collections}")
-    except Exception as e:
-        logger.error(f"Error initializing database: {str(e)}")
-        raise
+        self._dynamic_models: Dict[str, Type[Document]] = {}
+
+    async def initialize(self):
+
+        await init_beanie(database=self.db, document_models=self._static_models)
+
+    async def get_dynamic_model(
+        self, document_class: Type[TDocument], collection_name: str
+    ) -> Type[TDocument]:
+
+        # check if model is already initialized in dict
+        if self._dynamic_models.get(collection_name):
+            return self._dynamic_models[collection_name]
+        DynamicDocument = deepcopy(document_class)
+        
+        # Modify the Settings inner class
+        if hasattr(DynamicDocument, "Settings"):
+            DynamicDocument.Settings.name = collection_name
+        else:
+
+            class Settings:
+                name = collection_name
+
+            setattr(DynamicDocument, "Settings", Settings)
+
+        DynamicDocument.__name__ = f"{document_class.__name__}_{collection_name}"
+
+        await init_beanie(database=self.db, document_models=[DynamicDocument])
+
+        # add model to dict of initialized models
+        self._dynamic_models[collection_name] = DynamicDocument
+
+        return DynamicDocument
+
+    async def set_database(self, db_name: str):
+        self.db = self.client[db_name]
+        await self.initialize()
+        return self.db
