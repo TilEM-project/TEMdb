9904738b9df94db467a92b877dff729251a025b7
Derrick Brittain <dlbrittain@gmail.com>
2026-07-21 15:08:12 -0700

    feat: update_at is now null on create

--- packages/temdb/src/temdb/server/sqlmodels/base.py
+++ packages/temdb/src/temdb/server/sqlmodels/base.py
@@ -18,9 +18,8 @@ class TimestampMixin:
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), nullable=False, server_default=func.now(),
     )
-    updated_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), nullable=False,
-        server_default=func.now(), onupdate=func.now(),
+    updated_at: Mapped[datetime | None] = mapped_column(
+        DateTime(timezone=True), nullable=True, onupdate=func.now(),
     )
 
 
